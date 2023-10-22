501990ab7aaee547d7cc6c0fe0819b47d4d9a5fe
avetodef <sofiafedoteva@gmail.com>
2023-10-22 15:37:35 +0300

    lab 2 done

--- 2lab_example/gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require("gulp");
-
-gulp.task("hello", function (cb) {
-  console.log("Hello, Gulp!");
-  cb(); // Вызываем функцию обратного вызова, чтобы сигнализировать о завершении задачи
-});
